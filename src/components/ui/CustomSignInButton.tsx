6764d8de8e28e78d7f2079b1108448403dfec0dd
Jukelyn <viperkillergaming@gmail.com>
2025-07-18 14:33:02 -0400

    Fixed redundant styling

--- src/components/ui/CustomSignInButton.tsx
+++ src/components/ui/CustomSignInButton.tsx
@@ -3,19 +3,19 @@ import { SignInButton } from "@clerk/nextjs";
 const CustomSignInButton = () => {
   return (
     <div
-      className="relative cursor-pointer py-2 px-4 text-center font-barlow inline-flex justify-center text-base uppercase text-white rounded-lg border-solid transition-transform duration-300 ease-in-out group outline-offset-4 focus:outline focus:outline-2 focus:outline-white focus:outline-offset-4 overflow-hidden"
+      className="font-barlow group relative inline-flex cursor-pointer justify-center overflow-hidden rounded-lg border-solid px-4 py-2 text-center text-base text-white uppercase outline-offset-4 transition-transform duration-300 ease-in-out focus:outline-2 focus:outline-offset-4 focus:outline-white"
       onClick={() => {
         const btn = document.querySelector("[data-sign-in-button]");
         if (btn) (btn as HTMLElement).click();
       }}
     >
       <SignInButton data-sign-in-button />
-      <span className="absolute left-[-75%] top-0 h-full w-[50%] bg-white/20 rotate-12 z-10 blur-lg group-hover:left-[125%] transition-all duration-1000 ease-in-out"></span>
+      <span className="absolute top-0 left-[-75%] z-10 h-full w-[50%] rotate-12 bg-white/20 blur-lg transition-all duration-1000 ease-in-out group-hover:left-[125%]"></span>
 
-      <span className="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute h-[20%] rounded-tl-lg border-l-2 border-t-2 top-0 left-0"></span>
-      <span className="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute group-hover:h-[90%] h-[50%] rounded-tr-lg border-r-2 border-t-2 top-0 right-0"></span>
-      <span className="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute h-[50%] group-hover:h-[90%] rounded-bl-lg border-l-2 border-b-2 left-0 bottom-0"></span>
-      <span className="w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute h-[20%] rounded-br-lg border-r-2 border-b-2 right-0 bottom-0"></span>
+      <span className="drop-shadow-3xl absolute top-0 left-0 block h-[20%] w-1/2 rounded-tl-lg border-t-2 border-l-2 border-[#D4EDF9] transition-all duration-300"></span>
+      <span className="drop-shadow-3xl absolute top-0 right-0 block h-[50%] w-1/2 rounded-tr-lg border-t-2 border-r-2 border-[#D4EDF9] transition-all duration-300 group-hover:h-[90%]"></span>
+      <span className="drop-shadow-3xl absolute bottom-0 left-0 block h-[50%] w-1/2 rounded-bl-lg border-b-2 border-l-2 border-[#D4EDF9] transition-all duration-300 group-hover:h-[90%]"></span>
+      <span className="drop-shadow-3xl absolute right-0 bottom-0 block h-[20%] w-1/2 rounded-br-lg border-r-2 border-b-2 border-[#D4EDF9] transition-all duration-300"></span>
     </div>
   );
 };
